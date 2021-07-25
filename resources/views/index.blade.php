b1c3b7f8f09d954462bea6072029294c20989431
AllSafeCybercurity <skull32273227@gmail.com>
2021-07-25 13:11:31 -0700

    'Mig-21'

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+
+@section('page-content')
+    <section class="relative min-h-screen flex items-center">
+        <div class="container mx-auto text-center">
+            <h2 class="text-4xl sm:text-8xl">Fight <span class="text-pink-500">Club</span></h2>
+            <h3 class="text-2xl sm:text-4xl italic">with Tyler Durden</h3>         
+        <div class="absolute bottom-0 left-0 right-0 p-20">
+            <p class="center"></p>
+        </div>
+         
+    </section>
+@endsection
